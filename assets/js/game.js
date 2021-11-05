5dca41d9662d9ccc4b74fedcd447b426f2023ccc
ibromurph <ibrahim.murphy@hotmail.com>
2021-11-05 02:33:31 +0000

    Created const variables for the game buttons calling data attributes using query selectors

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,15 @@
 // GLOBAL CONSTANTS
 
-
 // GLOBAL VARIABLES
-
+let score = 0
 
 // HTML SELECTORS
+const startButton = document.querySelector('[data-button="start"]');
+const rulesButton = document.querySelector('[data-button="rules"]');
+const closeButton = document.querySelector('[data-button="close"]');
+const playAgainButton = document.querySelector('[data-button="play-again"]');
+
+const resultText = document.querySelector('[data-text="result"]'); 
 
 
 // EVENT LISTENERS
